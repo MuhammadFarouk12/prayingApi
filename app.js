dbf92521ff54497da67d43f7df4395bc15b2eae5
MuhammadFarouk12 <muhammad12fk@gmail.com>
2025-08-19 00:52:49 +0300

    Testing the API

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-function getTimes(city, countryCode){
-
+async function getTimes(city, countryCode){
+  console.log("Starting . . .")
+  const response = await fetch(`http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${countryCode}`)
+  const parsedResponse = await response.json();
+  console.log(parsedResponse.data.timings)
 }
+
+document.getElementById("getTimes").onclick = ()=> getTimes(city.value, code.value)
