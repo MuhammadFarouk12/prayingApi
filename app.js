227a985c2f10b80d899d1d002188a9340b52d49f
MuhammadFarouk12 <muhammad12fk@gmail.com>
2025-08-19 01:21:37 +0300

    Applying Functionality

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+city.focus();
 async function getTimes(city, countryCode){
   console.log("Starting . . .")
+  document.getElementById("show_city").innerText = city.toUpperCase() + " City "
+  document.getElementById("show_country").innerText = countryCode.toUpperCase() + " Country"
   const response = await fetch(`http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${countryCode}`)
   const parsedResponse = await response.json();
-  console.log(parsedResponse.data.timings)
+  const times = parsedResponse.data.timings
+  let container = ""
+  for (const time in times) {
+    console.log(time + " : " + times[time])
+    container += `<li>${time} : ${times[time]}</li>`
+  }
+    document.getElementById("times_container").innerHTML = container
 }
 
 document.getElementById("getTimes").onclick = ()=> getTimes(city.value, code.value)
